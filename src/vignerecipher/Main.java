28d2ebe037e280c42cd0891c159496cab231c374
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-02-04 18:54:06 +0700

    add InputFromFile and OutputToFile

--- src/vignerecipher/Main.java
+++ src/vignerecipher/Main.java
@@ -14,13 +14,21 @@ import java.awt.Panel;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JApplet;
 import javax.swing.JOptionPane;
-import vignerecipher.Vignere;
 
 /**
  *
- * @author Intel
+ * @author gmochid
  */
 public class Main extends JApplet implements ActionListener {
 
@@ -49,17 +57,20 @@ public class Main extends JApplet implements ActionListener {
         p0.setLayout(new GridLayout(1, 1));
         p0.add(judul);
 
-        p1.setLayout(new GridLayout(1, 2));
+        p1.setLayout(new GridLayout(1, 3));
         p1.add(inputLabel);
         p1.add(input);
+        p1.add(inputFromFileCB);
 
-        p2.setLayout(new GridLayout(1, 2));
+        p2.setLayout(new GridLayout(1, 3));
         p2.add(outputLabel);
         p2.add(output);
+        p2.add(outputToFileCB);
 
-        p3.setLayout(new GridLayout(1, 2));
+        p3.setLayout(new GridLayout(1, 3));
         p3.add(keyLabel);
         p3.add(key);
+        p3.add(new Panel());
 
         p4.setLayout(new GridLayout(1, 3));
         p4.add(asPlaintextCB);
@@ -90,41 +101,51 @@ public class Main extends JApplet implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
 
+        String tmpOutput = null;
+        StringBuilder buffOutput = new StringBuilder();
+        String tmpInput;
+        try {
+            tmpInput = getInput();
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this, "File input tidak ditemukan");
+            return;
+        }
+
+        // validation key
         if(key.getText().length() > 25) {
             JOptionPane.showMessageDialog(this, "Panjang key harus kurang dari 25");
             return;
         }
-
+        // validation encipher and decipher
         if((cmd.equals("Encipher")) || (cmd.equals("Decipher"))) {
             for (char i = '0'; i <= '9'; i++) {
                 StringBuffer tmp = new StringBuffer();
                 tmp.append(i);
-                if(input.getText().contains(tmp)) {
+                if((tmpInput.contains(tmp)) && (key.getText().contains(tmp))) {
                     JOptionPane.showMessageDialog(this, "Tidak boleh ada karakter angka dalam perintah 'Encipher dan 'Decipher'");
                     return;
                 }
             }
         }
-
-        String tmpOutput = null;
-        StringBuilder buffOutput = new StringBuilder();
+        
+        // encipher and decipher process
         if(cmd.equals("Encipher")) {
             if(key.getText().equals("")) {
-                tmpOutput = Vignere.encipher(input.getText());
+                tmpOutput = Vignere.encipher(tmpInput);
             } else {
-                tmpOutput = Vignere.encipher(input.getText(), key.getText());
+                tmpOutput = Vignere.encipher(tmpInput, key.getText());
             }
         } else if (cmd.equals("Decipher")) {
             if(key.getText().equals("")) {
-                tmpOutput = Vignere.decipher(input.getText());
+                tmpOutput = Vignere.decipher(tmpInput);
             } else {
-                tmpOutput = Vignere.decipher(input.getText(), key.getText());
+                tmpOutput = Vignere.decipher(tmpInput, key.getText());
             }
         } else if(cmd.equals("Encipher256")) {
             if(key.getText().equals("")) {
-                tmpOutput = Vignere.encipher256(input.getText());
+                tmpOutput = Vignere.encipher256(tmpInput);
             } else {
-                tmpOutput = Vignere.encipher256(input.getText(), key.getText());
+                tmpOutput = Vignere.encipher256(tmpInput, key.getText());
             }
         } else if (cmd.equals("Decipher256")) {
             if(key.getText().equals("")) {
@@ -133,8 +154,9 @@ public class Main extends JApplet implements ActionListener {
                 tmpOutput = Vignere.decipher256(input.getText(), key.getText());
             }
         }
+
+        // formatting output
         if(outputChoice.getSelectedCheckbox().getLabel().equals("As Plaintext")) {
-            String tmpInput = input.getText();
             int j = 0;
             for (int i = 0; i < tmpInput.length(); i++) {
                 if(tmpInput.charAt(i) == ' ') {
@@ -153,7 +175,34 @@ public class Main extends JApplet implements ActionListener {
             }
             tmpOutput = buffOutput.toString();
         }
-        output.setText(tmpOutput);
+
+        if(outputToFileCB.getState()) {
+            try {
+                PrintWriter out = new PrintWriter(output.getText());
+                out.println(tmpOutput);
+                out.close();
+            } catch (FileNotFoundException ex) {
+                JOptionPane.showMessageDialog(this, "File output tidak ditemukan");
+                return;
+            }
+        } else {
+            output.setText(tmpOutput);
+        }
+    }
+
+    private String getInput() throws IOException {
+        if(inputFromFileCB.getState()) {
+            BufferedReader br = new BufferedReader(new FileReader(input.getText()));
+            String line = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            String ls = System.getProperty("line.separator");
+            while ((line = br.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+            return stringBuilder.toString();
+        }
+        return input.getText();
     }
 
     // GUI Object
@@ -168,7 +217,7 @@ public class Main extends JApplet implements ActionListener {
     Checkbox asPlaintextCB = new Checkbox("As Plaintext", outputChoice, true);
     Checkbox noSpaceCB = new Checkbox("No Space", outputChoice, false);
     Checkbox fiveCharacterCB = new Checkbox("Five Character", outputChoice, false);
-
-
+    Checkbox inputFromFileCB = new Checkbox("Input From File", false);
+    Checkbox outputToFileCB = new Checkbox("Output To File", false);
 }
 
